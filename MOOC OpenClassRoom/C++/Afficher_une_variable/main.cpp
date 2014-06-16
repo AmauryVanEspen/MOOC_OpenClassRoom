36c063c9b52a22228093d592f74c97814c83b3b6
Amaury Van Espen <vanespen.amaury@gmail.com>
2014-06-16 19:08:32 +0200

    Mise à jour
    Mise à jour

---
+++ MOOC OpenClassRoom/C++/Afficher_une_variable/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    int ageUtilisateur(16);
+    int nombreAmis(432);      //Le nombre d'amis de l'utilisateur
+
+    double pi(3.14159);
+
+    bool estMonAmi(true);    //Cet utilisateur est-il mon ami ?
+
+    char lettre('a');
+
+    string nomUtilisateur("Albert Einstein");
+
+    cout << nomUtilisateur;
+    return 0;
+}
